386a386b0316bf749dbcefce18d81e6a9935ad32
nicourrrn <s57111702@gmail.com>
2024-05-08 20:46:34 +0300

    Create base of all modificable services

--- Task2/main-server/src/main.rs
+++ Task2/main-server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/ai")]
+async fn ai_call() -> String {
+    let response = reqwest::get("http://ai-service/").await.unwrap();
+    response.text().await.unwrap()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, ai_call])
 }
